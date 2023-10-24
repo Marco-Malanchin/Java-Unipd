69dd8c71e860a2811d864c0f24539c9cfcfd7fdc
Xmamo <72445677+Marco-Malanchin@users.noreply.github.com>
2023-10-24 09:45:29 +0200

    Esercizio 3 java

---
+++ Divisioni.java
@@ -0,0 +1,13 @@
+public class Divisioni{
+	public static void main(String [] args){
+		int m = 11;
+		int n = 5;
+		System.out.println("Il risultato della divisione tra il numero m e quello n e':" +m/n);
+		System.out.println("Il quoziente della divisione tra il numero m e quello n e':" +m%n);
+		
+		double x = 7.0;
+		double y = 0.0;
+		System.out.println("Il risultato della divisione tra il numero x e quello y e':" +x/y);
+		System.out.println("Il quoziente della divisione tra il numero m e quello n e':" +x%y);
+	}
+}
