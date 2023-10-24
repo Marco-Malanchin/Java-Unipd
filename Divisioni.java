a40c58b7c28db6ad0f77167534d44fdb570fc65d
Xmamo <72445677+Marco-Malanchin@users.noreply.github.com>
2023-10-24 10:05:24 +0200

    Update Divisioni.java

--- Divisioni.java
+++ Divisioni.java
@@ -1,13 +1,13 @@
 public class Divisioni{
 	public static void main(String [] args){
-		int m = 11;
+		int m = 11; //numeri Interi
 		int n = 5;
-		System.out.println("Il risultato della divisione tra il numero m e quello n e':" +m/n);
-		System.out.println("Il quoziente della divisione tra il numero m e quello n e':" +m%n);
+		System.out.println("Il risultato della divisione tra il numero m e quello n e':" +m/n); //stampo risultato divisione
+		System.out.println("Il quoziente della divisione tra il numero m e quello n e':" +m%n); //stampo quoziente divisione
 		
-		double x = 7.0;
+		double x = 7.0; //numeri reali
 		double y = 0.0;
-		System.out.println("Il risultato della divisione tra il numero x e quello y e':" +x/y);
-		System.out.println("Il quoziente della divisione tra il numero m e quello n e':" +x%y);
+		System.out.println("Il risultato della divisione tra il numero x e quello y e':" +x/y); //stampo risultato divisione che dara' infinity, -infinity o Nan a seconda dei casi
+		System.out.println("Il quoziente della divisione tra il numero m e quello n e':" +x%y); //stampo quoziente divisione che dara' sempre Nan
 	}
 }
