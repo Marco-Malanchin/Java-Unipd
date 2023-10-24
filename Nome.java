bc4a20a6219c0817a820acec408f5d77a4685c9f
Xmamo <72445677+Marco-Malanchin@users.noreply.github.com>
2023-10-24 10:02:53 +0200

    Es 5 java

---
+++ Nome.java
@@ -0,0 +1,6 @@
+public class Nome{
+	public static void main(String[] args){
+	System.out.println("Questo programma scrive il tuo nome incorniciato");
+	System.out.println(" ----------\n|  Marco   |\n ----------");	// Utilizzo \n per andare a capo nella stessa frase stampata senza utilizzare piu' println per andare a capo
+	}
+}
