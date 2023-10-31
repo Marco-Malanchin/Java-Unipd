417e14de74c137d80b753b47656e6dfe18eca98f
Xmamo <72445677+Marco-Malanchin@users.noreply.github.com>
2023-10-31 09:07:30 +0100

    es 8

---
+++ MediaNumeri.java
@@ -0,0 +1,11 @@
+public class MediaNumeri{
+	public static void main(String[] arg){
+		int n1 = 3;
+		int n2 = 5;
+		int n3 = 10;
+		int n4 = 11;
+		double totale = n1 + n2 + n3 + n4;
+		double media = totale/4;
+		System.out.println("la somma dei numeri e':" + totale + " la media dei numeri e':" + media);
+	}
+}
