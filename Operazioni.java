6557c2a73d3de5361ffe14649334a8885b44466f
Xmamo <72445677+Marco-Malanchin@users.noreply.github.com>
2023-10-31 09:07:48 +0100

    Es 9

---
+++ Operazioni.java
@@ -0,0 +1,12 @@
+public class Operazioni{
+	public static void main(String[] arg){
+		double n1, n2, n3, m1, m2, m3;
+        	n1 = 5;
+        	n2 = -9;
+        	n3 = 11;
+        	m1=n1*n2/n3;
+        	m2=n2/n3*n1;
+        	m3=n1/n3*n2;
+		System.out.println("numero m1:" + m1 + " numero m2:" + m2 + " numero m3:" + m3);
+	}
+}
