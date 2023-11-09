7e64f1c3ee79d8778829fa2354aa3e5e7785c6f6
Xmamo <72445677+Marco-Malanchin@users.noreply.github.com>
2023-11-09 22:17:25 +0100

    Update caratteriStringhe.java

--- caratteriStringhe.java
+++ caratteriStringhe.java
@@ -6,19 +6,34 @@ class caratteriStringhe {
         System.out.println("Inserisci la prima stringa");
         String frase1 = scan.nextLine();
         frase1 = frase1.toLowerCase();
-        System.out.println("Inserisci la seeconda stringa");
+		int lunghezza1 = frase1.lenght();
+		int lunghezza2;
+		do{
+		System.out.println("Inserisci la seconda stringa");
         String frase2 = scan.nextLine();
+		lunghezza2 = frase2.lenght();
+		}while(lunghezza2 >= lunghezza1);
         frase2 = frase2.toLowerCase();
-        int lunghezza1 = frase1.lenght();
-        int lunghezza2 = frase2.lenght();
+		boolean trovato;
         for (int i = 0; i < lunghezza1; i++){
-            for(int j = 0; j < lunghezza2; j++){
-                if(frase1.charAt(i) == frase2.charAt(j)){
-                    
+   			int j;         
+			for(j = 0; j < lunghezza2; j++){
+                if(frase1.charAt(i + j) != frase2.charAt(j)){
+                    break;
                 }
             }
+			if(j == lunghezza2){
+				trovato = true;
+			}
+			else{
+				trovato == false;
+			}
         }
-        
-        
+        if(trovato == true){
+			System.out.println("Stringa 2 e' una sotto stringa");
+		}
+		else{
+			System.out.println("Stringa 2 non e' una sotto stringa");
+		}
     }
 }
