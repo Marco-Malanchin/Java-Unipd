8dd51b8371b40d6a4d9b28c11f5b76d6cce0c8ee
Marco-Malanchin <72445677+Marco-Malanchin@users.noreply.github.com>
2023-11-28 13:22:50 +0100

    es2 lab 6

--- lab 6/InversoStringhe.java
+++ lab 6/InversoStringhe.java
@@ -14,15 +14,26 @@ public class InversoStringhe{
 			string2 = args[1];
 		}
 		else if(args.length == 0){
-			System.out.println("Inserisci prima stringa per fare il controllo");
+			System.out.println("Inserisci la prima stringa per fare il controllo");
 			string1 = scan.nextLine();
-			System.out.println("Inserisci seconda stringa per fare il controllo");
+			System.out.println("Inserisci la seconda stringa per fare il controllo");
 			string2 = scan.nextLine();
 		}
 		else if(args.length == 1){
-			System.out.println("Inserisci seconda stringa per fare il controllo");
+			System.out.println("Inserisci la seconda stringa per fare il controllo");
 			string2 = scan.nextLine();
 			string1 = args[0];
+		}
+		string1 = string1.toLowerCase();
+		string2 = string2.toLowerCase();
+		char[] s1 = InversoStringhe.toArray(string1);
+		char[] s2 = InversoStringhe.toArray(string2);
+		boolean controllo = InversoStringhe.reverseEquals(s1, s2);
+		if(controllo == true){
+			System.out.println("Le stringhe " + string1 + " e " + string2 + " sono  una l'inversa dell'altra");
+		}
+		else{
+			System.out.println("Le stringhe " + string1 + " e " + string2 + " non  sono  una l'inversa dell'altra");
 		}
 		scan.close();
 	}
@@ -33,5 +44,15 @@ public class InversoStringhe{
 			pippo[i] = s.charAt(i);
 		}
 		return pippo;
+	}
+	public static boolean reverseEquals(char[] a, char[] b){ 
+		boolean controllo = true;
+		for(int i = 0, j = b.length - 1; i < a.length && j >=  0;  i++, j-- ){
+			if (a[i] != b[j]){
+				controllo = false;
+				break;
+			}
+		}
+		return  controllo;
 	}
 }
